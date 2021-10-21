3cb196b3507ddb01283273ad80cafb681e189d87
Supriyo <72860690+Supsource@users.noreply.github.com>
2021-10-21 23:26:24 +0530

    import models and added face-api

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const video = document.getElementById('video')
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+]).then(startVideo)
+
+
+
+
 function startVideo(){
     navigator.getUserMedia(
         {video: {} },
@@ -7,5 +17,3 @@ function startVideo(){
         err => console.error(err)
     )
 }
-
-startVideo()
