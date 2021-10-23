6a11020a6d07caea0877f44fb9ec4a5998c67826
Supriyo <72860690+Supsource@users.noreply.github.com>
2021-10-23 22:18:03 +0530

    faceapi.detectAllFaces done everything :100:

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ Promise.all([
     faceapi.nets.faceExpressionNet.loadFromUri('/models'),
 ]).then(startVideo)
 
-
-
-
 function startVideo(){
     navigator.getUserMedia(
         {video: {} },
@@ -17,3 +14,19 @@ function startVideo(){
         err => console.error(err)
     )
 }
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height}
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    
+    }, 100)
+})
